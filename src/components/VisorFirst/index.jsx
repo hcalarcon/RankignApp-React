e6af1b05ec77bfbe8d8f28fc827b9e4f6549495a
hcalarcon <hernaldo.alar@gmail.com>
2024-09-10 18:38:08 -0300

    mejoras

--- src/components/VisorFirst/index.jsx
+++ src/components/VisorFirst/index.jsx
@@ -5,17 +5,22 @@ const VisorFrist = ({ primeros, isLoading, title = "6° 5°" }) => {
   console.log(primeros);
 
   const orden = [2, 1, 3];
+  const reorderedPrimeros = [
+    primeros[1] || {}, // Second article
+    primeros[0] || {}, // First article
+    primeros[2] || {}, // Third article
+  ];
   return (
     <section className={style.visor}>
       {isLoading ? (
         <CircularProgress />
       ) : (
         <>
-          <p className={style.rankingTitle}>6to</p>
-          {primeros.map((nombre, index) => (
+          <p className={style.rankingTitle}>6to 5ta</p>
+          {reorderedPrimeros.map((nombre, index) => (
             <article key={index}>
               <p>{orden[index]}</p>
-              <p>{primeros[index].nombre}</p>
+              <p>{nombre.nombre}</p>
             </article>
           ))}
         </>
